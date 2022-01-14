cb311b3fbdbb1cc900fab4debc19663b42093df6
KristenSoroka <>
2022-01-14 18:10:37 +0000

    Let the player know the correct answer if they got it wrong.
    Saving my commit here.  My next task is to add a scoring system.

--- main.py
+++ main.py
@@ -17,26 +17,26 @@ ans = int(input("What is Taylor's favorite number? "))
 if ans == 13:
   print("Correct!")
 else: 
-  print("Incorrect!")
+  print("Incorrect! The correct answer is 13")
 
 # Q2
 ans = int(input("How many cats does she own? "))
 if ans == 3:
   print("Correct!")
 else: 
-  print("Incorrect!")
+  print("Incorrect! The correct answer is 3")
 
 # Q3
 ans = input("Finish this lyric: 'Darling, I\'m a nightmare...' ").lower()
 if ans == "dressed like a daydream":
   print("Correct!")
 else: 
-  print("Incorrect!")
+  print("Incorrect! The correct answer is 'dressed like a daydream'")
 
 #Q4
 ans = input("What is Taylor's middle name? ").capitalize()
 if ans == "Alison":
   print("Correct!")
 else: 
-  print("Incorrect!")
+  print("Incorrect! The correct answer is Alison")
 
